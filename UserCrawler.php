1f5c37e04998a88f381c3a08e6dabd77a9f67adb
刘义 <forehalo@gmail.com>
2015-10-24 16:06:18 +0800

    user

---
+++ UserCrawler.php
@@ -0,0 +1,17 @@
+<?php namespace Crawler;
+require_once 'simple_html_dom.php';
+
+use simple_html_dom;
+
+/**
+ * Class UserCrawler.php
+ * @package     Crawler
+ * @version     1.0.0
+ * @copyright   Copyright (c) 2015 forehalo <http://www.forehalo.top>
+ * @author      forehalo <forehalo@gmail.com>
+ * @license     http://www.gnu.org/licenses/lgpl.html   LGPL
+ */
+class UserCrawler
+{
+
+}
