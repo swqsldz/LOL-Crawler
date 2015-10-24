612558a751516a74b2991164ca92a9090f006756
刘义 <forehalo@gmail.com>
2015-10-24 16:07:04 +0800

    get base information

--- UserCrawler.php
+++ UserCrawler.php
@@ -35,4 +35,36 @@ class UserCrawler
         }
         return $serverName == '' ? $servers : $servers[$serverName];
     }
+
+    public function baseInfo($name, $serverName){
+        $userInfo = [];
+        $keys = ['场次','胜率'];
+        $url = $this->search($name, $serverName);
+
+        $handle = curl_init($url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_exec($handle);
+
+        $info = curl_getinfo($handle);
+        $url = $info['redirect_url'];
+
+        curl_setopt($handle, CURLOPT_URL, $url);
+        $page = curl_exec($handle);
+
+        $html = new simple_html_dom();
+        $html->load($page);
+
+        $userInfo['rank'] = $html->find('.info-attention span', 0)->plaintext;
+        $s5 = preg_split('/\s+/',trim($html->find('.location-list', 0)->children(0)->children(1)->children(1)->plaintext));
+        array_pop($s5);
+        $userInfo['complex']['S5排位赛'] = array_combine($keys, $s5);
+        $userInfo['complex']['S4排位赛']['场次'] = $html->find('.various-tips', 0)->plaintext;
+        $userInfo['complex']['S4排位赛']['胜率'] = $html->find('.various-tips', 1)->plaintext;
+        $userInfo['complex']['匹配赛']['场次'] = $html->find('.various-tips', 2)->plaintext;
+        $userInfo['complex']['匹配赛']['胜率'] = $html->find('.various-tips', 3)->plaintext;
+        $userInfo['complex']['大乱斗']['场次'] = $html->find('.various-tips', 4)->plaintext;
+        $userInfo['complex']['大乱斗']['胜率'] = $html->find('.various-tips', 5)->plaintext;
+
+        return $userInfo;
+    }
 }
